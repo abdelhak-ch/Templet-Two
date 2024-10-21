da9d3c5e083140623dccfb57cf9af3cff320349f
abdelhak-ch <163315420+abdelhak-ch@users.noreply.github.com>
2024-10-21 11:19:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,33 @@ let myRequest = new XMLHttpRequest();
 myRequest.open("GET", "https://api.github.com/users/abdelhak-ch/repos");
 myRequest.send();
 myRequest.onreadystatechange = function() {
-    print(myRequest.status);
-    print(myRequest.readyState);
     if (this.readyState === 4 && this.status === 200) {
-        print("hello world");
-        let jsonData = JSON.parse(this.responseText)
-        print(this.readyState);
-        print(this.responeText);
+        let jsonData = JSON.parse(this.responseText);
         for (let i=0; i < jsonData.length; i++) {
-            print(jsonData[i])
+            if ( jsonData[i].name !== "Templet-Two" && jsonData[i].name !== "Kasper" && jsonData[i].name !== "localStorage") {
+              print(jsonData[i].name);
+              open(`https://abdelhak-ch.github.io/${jsonData[i].name}/`);
+            }
         }
     }
-} 
-
+}
 function print(value) {
     let content = document.createTextNode(value)
     let element = document.createElement("div")
     element.appendChild(content)
     document.body.appendChild(element)
+}
+
+function open(url) {
+    let div = document.createElement("div");
+    document.body.appendChild(div);
+    div.innerHTML = `<iframe 
+          width="800" 
+          height="1000" 
+          src="${url}" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen>
+      </iframe>`
+    
 }
